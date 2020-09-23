ffa2e514644083d9b84b8ac91801488aa001a485
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-09-23 08:12:17 -0600

    Clase Persona

--- ProyectoDatos/Persona.h
+++ ProyectoDatos/Persona.h
@@ -1,12 +1,29 @@
 #pragma once
 #include <sstream>
+#include <iostream>
+using std::ostream;
 using std::string;
 using std::stringstream;
 class Persona
 {
+public:
+	string getNombre();
+	string getCed();
+	int getEdad();
+	void setEdad(int);
+	void setNombre(string);
+	void setCedula(string);
+	Persona();
+	Persona(string = "", string = "", int = 0);
+	friend ostream& operator <<(ostream&, Persona&);
+	bool operator == (Persona&);
+	bool operator > (Persona&);
+	bool operator < (Persona&);
+	bool operator >= (Persona&);
 private:
 	string nombre;
 	string ced;
 	int edad;
+	
 };
 
